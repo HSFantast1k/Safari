16a37d0d4a61cdc38442f2f8641cc4f47fdd990d
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-08 15:09:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(function(){
+	$('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
+		fixedContentPos: false
+	});
+
+    $('.gallery__slider').slick({
+        prevArrow: '<button class="slick-btn slick-prev" type="button"><img src="images/arrow-left.svg" alt="prew"></button>',
+        nextArrow: '<button class="slick-btn slick-next" type="button"><img src="images/arrow-right.svg" alt="next"></button>',
+    });
+
+    $('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
 });
